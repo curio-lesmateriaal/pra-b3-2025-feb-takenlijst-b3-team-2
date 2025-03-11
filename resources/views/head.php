f875f5c8e3da56df6a2209a29a811db48f9f3173
uitgeteld <swendewit07@gmail.com>
2025-03-11 10:15:20 +0100

    Added additional stylings and font
    Added additional styling to the header and added a font

--- resources/views/head.php
+++ resources/views/head.php
@@ -4,5 +4,9 @@ global $base_url;
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="<?php echo $base_url; ?>/css/normalize.css">
-<link rel="stylesheet" href="<?php echo $base_url; ?>/css/main.css">
+<link rel="stylesheet" href="./css/normalize.css">
+<link rel="stylesheet" href="./css/main.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Solway:wght@300;400;500;700;800&display=swap" rel="stylesheet">
