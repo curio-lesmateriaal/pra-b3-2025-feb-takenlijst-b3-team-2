111a03f25e3ffbeea4f15c5750e0bd40224a16a3
Royboot <rgrizell467@gmail.com>
2025-03-17 10:27:20 +0100

    Added takenlijst page
    added takenlijst page and start of html structure

---
+++ takenlijst.php
@@ -0,0 +1,50 @@
+<?php
+global $base_url;
+
+require_once 'config/config.php';
+
+?>
+
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <title></title>
+    <?php require_once "./resources/views/head.php"; ?>
+</head>
+
+<body>
+
+    <?php require_once "./resources/views/header.php"; ?>
+
+    <main class="container takenlijst">
+        <div class="flex-container" id="columns">
+            <div class="column">
+                <ul class="taken">
+                    <li>Taak1</li>
+                </ul>
+            </div>
+            <div class="column">
+                <ul class="taken">
+                    <li>Taak1</li>
+                    <li>Taak2</li>
+                </ul>
+            </div>
+            <div class="column">
+                <ul class="taken">
+                    <li>Taak1</li>
+                </ul>
+            </div>
+            <div class="column">
+                <ul class="taken">
+                    <li>Taak1</li>
+                </ul>
+            </div>
+        </div>
+    </main>
+
+    <?php require_once "./resources/views/footer.php"; ?>
+
+</body>
+
+</html>
