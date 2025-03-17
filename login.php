c044c8df427fc351652decd80f4479a74f6e5ce5
Royboot <rgrizell467@gmail.com>
2025-03-17 09:46:00 +0100

    Added login form
    added basic login form

--- login.php
+++ login.php
@@ -11,7 +11,13 @@ require_once './config/config.php';
     <?php require_once "./resources/views/header.php"; ?>
     <main>
         <form>
-
+            <label for="username">Gebruikersnaam:</label>
+            <input type="text" id="username" name="username" required>
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" required>
+            <label for="password">Wachtwoord</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit">Login</button>
         </form>
     </main>
     <?php require_once "./resources/views/footer.php" ?>
