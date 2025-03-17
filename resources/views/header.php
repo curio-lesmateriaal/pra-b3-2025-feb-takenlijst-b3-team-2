d2e8ff0667b3c278bb3bfec5152f4d937ebe9ec9
uitgeteld <swendewit07@gmail.com>
2025-03-17 09:56:48 +0100

    Fixed routing
    Fixed the routing to other files

--- resources/views/header.php
+++ resources/views/header.php
@@ -3,6 +3,6 @@
         <img src="./resources/img/icons/favicon.ico?" alt="icon">
         <a href="">Home</a>
         <a href="">Takenlijst</a>
-        <a href="./resources/views/login.php">Login</a>
+        <a href="../login.php">Login</a>
     </nav>
 </header>
