568fcf87afc4a8b8d7497b0c0fdcbedc76c2d9b8
uitgeteld <swendewit07@gmail.com>
2025-03-17 09:22:57 +0100

    Fixed header styling
    Fixed the header styling + changed the image to the Developer Land image

--- resources/views/header.php
+++ resources/views/header.php
@@ -1,10 +1,8 @@
 <header>
-    <nav>
-        <ul class="solway-bold">
-            <li><img src="./resources/img/icons/favicon.ico" alt="icon"></li>
-            <li><a href="">Home</a></li>
-            <li><a href="">Takenlijst</a></li>
-            <li><a href="">Login</a></li>
-        </ul>
+    <nav class="solway-bold">
+        <img src="./resources/img/icons/favicon.ico" alt="icon">
+        <a href="">Home</a>
+        <a href="">Takenlijst</a>
+        <a href="">Login</a>
     </nav>
 </header>
