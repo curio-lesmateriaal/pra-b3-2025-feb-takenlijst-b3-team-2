36fa1fa4ca410a79da72e49b0fc5af126bea1634
Royboot <rgrizell467@gmail.com>
2025-03-17 09:59:30 +0100

    changes to Login controller
    Added session vars to the Login controller

--- app/Http/Controlers/Auth/logincontroller.php
+++ app/Http/Controlers/Auth/logincontroller.php
@@ -8,10 +8,8 @@ $password = $_POST['password'];
 
 $query = "SELECT * FROM users WHERE username = ':username'";
 $stmt = $conn->prepare($query);
-$stmt->execute
-([
-    ':username' => $username,
-]);
+$stmt->bindParam(':username', $username);
+$stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($user->rowCount() == 0 or !password_verify($password, $user['password']))
@@ -23,8 +21,9 @@ if ($user->rowCount() == 0 or !password_verify($password, $user['password']))
 session_start();
 
 $_SESSION['id'] = $user['id'];
-$_SESSION['usr'] = $user['username'];
+$_SESSION['username'] = $user['username'];
+$_SESSION['is_logged_in'] = true;
 
 // TODO add a header to where the user page will be
-
+header('location: '.$base_url.'/index.php');
 exit;
