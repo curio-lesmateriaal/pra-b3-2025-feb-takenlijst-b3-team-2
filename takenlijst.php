c4b5549793a463e7ea4382d9da92eda000266f46
Royboot <rgrizell467@gmail.com>
2025-03-18 10:19:50 +0100

    stylinge changes+moved register page

--- takenlijst.php
+++ takenlijst.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-if(!isset($_SESSION['is_logged_in'])){
+if (!isset($_SESSION['is_logged_in'])) {
     $_SESSION['is_logged_in'] = false;
 }
-if($_SESSION['is_logged_in'] == false){
-    header('location: '.$base_url.'/login.php');
+if ($_SESSION['is_logged_in'] == false) {
+    header('location: ' . $base_url . '/login.php');
     exit;
 }
 global $base_url;
@@ -27,22 +27,27 @@ require_once 'config/config.php';
     <main class="container takenlijst">
         <div class="flex-container" id="columns">
             <div class="column">
-                <ul class="taken">
-                    <li>Taak1</li>
-                </ul>
-            </div>
-            <div class="column">
+                <h2>To-do</h2>
                 <ul class="taken">
                     <li>Taak1</li>
                     <li>Taak2</li>
                 </ul>
             </div>
             <div class="column">
+                <h2>Doing</h2>
+                <ul class="taken">
+                    <li>Taak1</li>
+                    <li>Taak2</li>
+                </ul>
+            </div>
+            <div class="column">
+                <h2>In-Review</h2>
                 <ul class="taken">
                     <li>Taak1</li>
                 </ul>
             </div>
             <div class="column">
+                <h2>Done</h2>
                 <ul class="taken">
                     <li>Taak1</li>
                 </ul>
