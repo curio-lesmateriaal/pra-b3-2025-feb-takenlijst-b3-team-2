8b3d4cd5cbcf34d1eac58b35997129a75021e80d
Royboot <rgrizell467@gmail.com>
2025-03-18 09:13:36 +0100

    fix registering

---
+++ app/Http/Controllers/Auth/registercontroller.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+global $conn, $base_url;
+
+require_once '../../../../config/conn.php';
+
+$username = isset($_POST['username']) ? $_POST['username'] : null;
+$password = isset($_POST['password']) ? $_POST['password'] : null;
+$email = isset($_POST['email']) ? $_POST['email'] : null;
+
+if ($username == null or $password == null or $email == null) {
+    header('location: ' . $base_url . '/index.php');
+    die("Error: please fill out the form!");
+}
+$password_hash = password_hash($password, PASSWORD_DEFAULT);
+$query = "INSERT INTO users (`email`,`username`, `password`) VALUES (:username, :password, :email)";
+$stmt = $conn->prepare($query);
+$stmt->bindParam(':username', $username);
+$stmt->bindParam(':password',$password_hash);
+$stmt->bindParam(':email', $email);
+$stmt->execute();
+
+$_SESSION['username'] = $username;
+$_SESSION['email'] = $email;
+$_SESSION['password'] = $password_hash;
+$_SESSION['is_logged_in'] = true;
+// TODO add a header to where the user page will be
+header('location: ' . $base_url . '/index.php');
+exit;
