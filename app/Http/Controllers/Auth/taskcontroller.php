f61d0dce11cf170a7ebe8bf31da14a09fdea66e4
Royboot <rgrizell467@gmail.com>
2025-04-01 10:06:34 +0200

    update

--- app/Http/Controllers/Auth/taskcontroller.php
+++ app/Http/Controllers/Auth/taskcontroller.php
@@ -2,11 +2,11 @@
 $user_id = isset($_SESSION['id']) ? $_SESSION['id'] : null;
 if($user_id == null)
 {
-    header('location:index.php');
+    header('location: '.$base_url.'/login.php');
     die("Error: please log in!");
 }
 require_once __DIR__ . '/../../../../config/conn.php'; 
-$sql = "SELECT * FROM taken WHERE user= :user_id";
+$sql = "SELECT * FROM taken WHERE user = :user_id";
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':user_id', $user_id);
 $stmt->execute();
--- takenlijst.php
+++ takenlijst.php
@@ -31,51 +31,48 @@ if ($_SESSION['is_logged_in'] == false) {
         <div class="flex-container" id="columns">
             <div class="column">
                 <h2>To-do</h2>
-                <ul class="taken">
-                    <?php
-                    foreach($tasks as $task) {
-                        if ($task['status'] == 'To-do') {
-                            echo "<li>" . $task['name'] . "</li>";
-                        }
-                    }
-                    ?>
-                </ul>
-            </div>
-            <div class="column">
-                <h2>Doing</h2>
-                <ul class="taken">
-                    <?php
-                    foreach($tasks as $task) {
-                        if ($task['status'] == 'doing') {
-                            echo "<li>" . $task['name'] . "</li>";
-                        }
-                    }
-                    ?>
-                </ul>
-            </div>
-            <div class="column">
-                <h2>In-Review</h2>
-                <ul class="taken">
-                    <?php
-                    foreach($tasks as $task) {
-                        if ($task['status'] == 'in-Review') {
-                            echo "<li>" . $task['name'] . "</li>";
-                        }
-                    }
-                    ?>
-                </ul>
-            </div>
-            <div class="column">
-                <h2>Done</h2>
-                <ul class="taken">
-                    <?php
-                    foreach($tasks as $task) {
-                        if ($task['status'] == 'done') {
-                            echo "<li>" . $task['name'] . "</li>";
-                        }
-                    }
-                    ?>
-                </ul>
+                 <ul class="taken">
+                     <?php
+                     $todo = array_filter($tasks, fn($task) => $task['status'] == 'To-do');
+                     $todo = array_filter($tasks, fn($task) => $task['status'] == 'to-do');
+ 
+                     foreach($todo as $task) {
+                         echo "<li>" . $task['name'] . "</li>";
+                         echo "<li>" . $task['titel'] . "</li>";
+                     }
+                     ?>
+                 </ul>
+ @@ -48,7 +48,7 @@
+                     $doing = array_filter($tasks, fn($task) => $task['status'] == 'doing');
+ 
+                     foreach($doing as $task) {
+                         echo "<li>" . $task['name'] . "</li>";
+                         echo "<li>" . $task['titel'] . "</li>";
+                     }
+                     ?>
+                 </ul>
+ @@ -57,10 +57,10 @@
+                 <h2>In-Review</h2>
+                 <ul class="taken">
+                     <?php
+                     $Review = array_filter($tasks, fn($task) => $task['status'] == 'in-Review');
+                     $Review = array_filter($tasks, fn($task) => $task['status'] == 'review');
+ 
+                     foreach($Review as $task) {
+                         echo "<li>" . $task['name'] . "</li>";
+                         echo "<li>" . $task['titel'] . "</li>";
+                     }
+                     ?>
+                 </ul>
+ @@ -72,7 +72,7 @@
+                     $done = array_filter($tasks, fn($task) => $task['status'] == 'done');
+ 
+                     foreach($done as $task) {
+                         echo "<li>" . $task['name'] . "</li>";
+                         echo "<li>" . $task['titel'] . "</li>";
+                     }
+                     ?>
+                 </ul>
             </div>
         </div>
     </main>
