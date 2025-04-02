648c53a8a8bef0231f29e12483f6f123152d2131
Royboot <rgrizell467@gmail.com>
2025-04-02 09:21:35 +0200

    Added logout option

--- resources/views/header.php
+++ resources/views/header.php
@@ -3,6 +3,9 @@
         <img src="<?php echo $base_url; ?>/resources/img/icons/favicon.ico?" alt="icon">
         <a href="<?php echo $base_url; ?>/index.php">Home</a>
         <a href="<?php echo $base_url; ?>/takenlijst.php">Takenlijst</a>
-        <a href="<?php echo $base_url; ?>/login.php">Login</a>
+        <a href="<?php echo $base_url; ?>/login.php" style="display:<?php echo $b = $_SESSION['is_logged_in']? "none": "inline"?>">Login</a>
+        <form class = "logout-form" action="<?php echo $base_url;?>/app/Http/Controllers/Auth/logoutcontroller.php">
+            <button class = "logout-button bold" style="display:<?php echo $b = $_SESSION['is_logged_in']? "inline": "none"?>" type="submit" class="logout">Logout</button>
+        </form>
     </nav>
 </header>
