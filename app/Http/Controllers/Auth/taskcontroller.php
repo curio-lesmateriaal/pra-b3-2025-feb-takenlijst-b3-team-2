b2668b95f080360c407d476063071c4579f79597
dysshanks <ryanvdvorst@outlook.com>
2025-04-02 09:57:12 +0200

    edit.php

--- app/Http/Controllers/Auth/taskcontroller.php
+++ app/Http/Controllers/Auth/taskcontroller.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 $user_id = isset($_SESSION['id']) ? $_SESSION['id'] : null;
 if($user_id == null)
 {
     header('location: '.$base_url.'/login.php');
     die("Error: please log in!");
 }
+<<<<<<< Updated upstream
 require_once __DIR__ . '/../../../../config/conn.php'; 
 if(isset($_POST['action'])||isset($_SESSION['action'])) {
     if($_SESSION['action'] == "select"||$_POST['action'] == "select"){
@@ -14,6 +15,11 @@ if(isset($_POST['action'])||isset($_SESSION['action'])) {
         $stmt->execute();
         $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }elseif($_POST['action'] == 'create') {
+=======
+require_once __DIR__ . '/../../../../config/conn.php';
+if(isset($_POST['action'])) {
+    if($_POST['action'] == 'create') {
+>>>>>>> Stashed changes
         $title = $_POST['title'];
         $description = $_POST['description'];
         $afdeling = $_POST['afdeling'];
@@ -32,6 +38,41 @@ if(isset($_POST['action'])||isset($_SESSION['action'])) {
             echo "Error creating task.";
         }
     }elseif($_POST['action'] == 'update'){
+<<<<<<< Updated upstream
+=======
+
+        $title = $_POST["title"];
+        $content = $_POST["bescrijving"];
+        $department = $_POST["department"];
+        $status = $_POST["status"];
+        $deadline = $_POST["deadline"];
+
+        if (empty($title) || empty($content) || empty($department) || empty($status) || empty($deadline))
+        {
+            header('location: '.$base_url.'/edit.php?$id');
+            die("Error: please fill out the form!");
+        }
+
+        $sql = "UPDATE taken SET titel =':titel', bescrijving = ':bescrijving', afdeling = ':afdeling', status = ':status', deadline='deadline'  WHERE id = ':id'";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':titel', $title);
+        $stmt->bindParam(':bescrijving', $content);
+        $stmt->bindParam(':afdeling', $department);
+        $stmt->bindParam(':status', $status);
+        $stmt->bindParam(':deadline', $deadline);
+        $stmt->bindParam(':id', $user_id);
+        $stmt->execute();
+
+        header('location: '.$base_url.'/takenlijst.php');
+
+
+    }elseif($_POST['action'] == "select"){
+        $sql = "SELECT * FROM taken WHERE user = :user_id";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+>>>>>>> Stashed changes
     }elseif($_POST['action'] == "delete"){
     }
 }
