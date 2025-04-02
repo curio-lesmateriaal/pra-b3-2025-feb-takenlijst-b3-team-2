b5e76e3075eeea0d33d3f1b32167ae21da331a30
Royboot <rgrizell467@gmail.com>
2025-04-02 10:07:39 +0200

    Added user id to session on register

--- app/Http/Controllers/Auth/registercontroller.php
+++ app/Http/Controllers/Auth/registercontroller.php
@@ -12,14 +12,18 @@ if ($username == null or $password == null or $email == null) {
     header('location: ' . $base_url . '/index.php');
     die("Error: please fill out the form!");
 }
+
 $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
 $query = "INSERT INTO users (`email`,`username`, `password`) VALUES (:email,:username ,:password)";
 $stmt = $conn->prepare($query);
 $stmt->bindParam(':username', $username);
 $stmt->bindParam(':password',$password_hash);
 $stmt->bindParam(':email', $email);
 $stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+$_SESSION['user_id'] = $user['id'];
 $_SESSION['username'] = $username;
 $_SESSION['email'] = $email;
 $_SESSION['password'] = $password_hash;
