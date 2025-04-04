1e5930054dda0b17cdd3dbc4deab5d7838848872
Royboot <rgrizell467@gmail.com>
2025-04-04 09:14:09 +0200

    Added check for session

--- app/Http/Controllers/Auth/taskcontroller.php
+++ app/Http/Controllers/Auth/taskcontroller.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if(session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
 global $conn, $base_url;
 $_SESSION[' error'] = "";
